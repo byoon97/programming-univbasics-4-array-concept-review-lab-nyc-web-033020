29e86505203ceb9a6a97455c1be48b66d79a9fc7
Brandon Yoon <byoon5397@gmail.com>
2020-03-23 07:20:14 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,7 +10,17 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
+  max = nil
+  i = 0
+  while i < array.length do
+    if array[i] > max
+      max = array[i]
+    end
 
+    i += 1
+  end
+
+  return max
 end
 
 def find_min_value(array)
